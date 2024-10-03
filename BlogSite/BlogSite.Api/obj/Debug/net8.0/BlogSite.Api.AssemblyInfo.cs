c261f0f138c14deb4b68b76ff975f2141183d475
Eric Bong <ericbong6868@gmail.com>
2024-10-03 11:47:27 +0800

    Updated ui

--- BlogSite/BlogSite.Api/obj/Debug/net8.0/BlogSite.Api.AssemblyInfo.cs
+++ BlogSite/BlogSite.Api/obj/Debug/net8.0/BlogSite.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogSite.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b33a0d86300ca5e0c2cf19e50ecb9c4f7bdceed3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ccdb500f1e2045e44862673c1202dc074f82617")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogSite.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogSite.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
